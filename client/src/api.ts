7b44f75c4351a37adcb4491a851e0f217fded7b3
Brian Devadason <briandevml@gmail.com>
2023-04-15 00:10:45 -0400

    Making required changes to the client file. This will allow the client to open on the internet browser at http://127.0.0.1:5000/

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,6 +1,6 @@
 import { OutputParams } from './params';
 
-const server = 'https://lofiserver.jacobzhang.de';
+const server = 'http://127.0.0.1:5000';
 
 export const generate = (): Promise<OutputParams> =>
   fetch(`${server}/generate`)
